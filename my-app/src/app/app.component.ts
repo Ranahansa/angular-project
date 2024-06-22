9c5389a0faf3394165cc601ff863149cae7b54e5
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-06-22 16:21:56 +0530

    Update app component to include header and footer components

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HomeComponent]
+    imports: [RouterOutlet, HomeComponent, FooterComponent, HeaderComponent]
 })
 export class AppComponent {
   title = 'my-app';
